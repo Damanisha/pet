d6a47ca28c78408ca5c7396e6f3e643aa258e8b2
Damanisha <cristobalmarkadrian@gmail.com>
2024-06-02 13:31:07 +0800

    Update dbcnp.sql

--- database/dbcnp.sql
+++ database/dbcnp.sql
@@ -11,14 +11,8 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dbcnp`
+-- 
+-- Database: `strayweb`
 --
 
 -- --------------------------------------------------------
@@ -28,23 +22,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admininfo` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `contact` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `username` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL
+  `password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-
-
--- alter tbldoginfo, modify id column --
-
-ALTER TABLE tblDogInfo
-MODIFY COLUMN id varchar(20) NOT NULL;
-
---
 -- Dumping data for table `admininfo`
 --
 
@@ -54,39 +40,36 @@ INSERT INTO `admininfo` (`id`, `name`, `contact`, `email`, `username`, `password
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblcnp`
+-- Table structure for table `tblDogInfo`
 --
 
-CREATE TABLE `tblcnp` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `prize` float DEFAULT NULL,
-  `description` varchar(50) DEFAULT NULL,
-  `image` varchar(250) DEFAULT NULL,
-  `status` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
--- Table structure for dog information
-
 CREATE TABLE `tblDogInfo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
   `breed` varchar(50) DEFAULT NULL,
   `gender` varchar(10) DEFAULT NULL,
-  `physical_characteristics` varchar(255) DEFAULT NULL,
-  `medical_records` varchar(255) DEFAULT NULL,
-  `location` varchar(100) DEFAULT NULL,
+  `color_markings` varchar(255) NOT NULL,
+  `location_of_captivity` varchar(100) DEFAULT NULL,
   `date` date DEFAULT NULL,
   `time` time DEFAULT NULL,
-  `location_of_captivity` varchar(100) DEFAULT NULL,
-  `dog_pictures` varchar(250) DEFAULT NULL,
+  `remarks_description` varchar(100) DEFAULT NULL,
+  `dog_pictures` text NOT NULL,
+  `last_vaccination_date` date DEFAULT NULL,
+  `residence_last_3_months` varchar(255) DEFAULT NULL,
+  `has_owner` varchar(10) NOT NULL,
   `owner_id` int(11) DEFAULT NULL,
+  `prediction` varchar(255) DEFAULT NULL,
+  `confidence` float DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `owner_id` (`owner_id`),
   CONSTRAINT `tblDogInfo_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `tblOwnerInfo` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Table structure for dog owner information --
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblOwnerInfo`
+--
 
 CREATE TABLE `tblOwnerInfo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -99,109 +82,20 @@ CREATE TABLE `tblOwnerInfo` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
--- edit columns --
-
-ALTER TABLE tblDogInfo
-CHANGE COLUMN `location_of_captivity` `remarks_description` VARCHAR(100) DEFAULT NULL,
-CHANGE COLUMN `location` `location_of_captivity` VARCHAR(100) DEFAULT NULL;
-
--- Dumping data for table `tblcnp`
+-- Additional indexes and auto_increment for tables
 --
 
-INSERT INTO `tblcnp` (`id`, `name`, `prize`, `description`, `image`, `status`) VALUES
-(1, 'Baltimore Oriole', 350, 'The rich, whistling song of the Baltimore Oriole, ', 'images/e.jpg', 'Available'),
-(2, 'Maltese', 500, 'A dog breed whoâ€™s gentle and fearless, the Malte', 'images/2.jpg', 'Available'),
-(3, 'Bulldog', 550, 'Bulldogs originally were used to drive cattle to m', 'images/3.jpg', 'Available'),
-(4, 'Gordon Setter', 600, 'The Gordon Setter, the black avenger of the Highla', 'images/5.jpg', 'Available'),
-(5, 'Bedlington Terrier', 500, 'Bedlingtons are lithe, energetic Englishmen. The c', 'images/9.JPG', 'Available');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblorders`
---
-
-CREATE TABLE `tblorders` (
-  `id` int(11) NOT NULL,
-  `cname` varchar(50) DEFAULT NULL,
-  `address` varchar(100) DEFAULT NULL,
-  `contact` varchar(50) DEFAULT NULL,
-  `cnpoid` int(11) DEFAULT NULL,
-  `oqty` int(11) DEFAULT NULL,
-  `ostatus` varchar(50) DEFAULT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `otype` varchar(50) DEFAULT NULL,
-  `datepickup` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tblorders`
---
-
-INSERT INTO `tblorders` (`id`, `cname`, `address`, `contact`, `cnpoid`, `oqty`, `ostatus`, `timestamp`, `otype`, `datepickup`) VALUES
-(10, 'Jeancen Sayoc', 'Tacloban City', '09369420867', 4, 1, 'new', '2016-10-13 15:09:46', 'Pick-up', '2016-10-14'),
-(11, 'Jhazel Dela Cruz', 'Kabankalan City', '09095744586', 3, 3, 'new', '2019-03-17 23:15:46', 'Pick-up', '2019-03-18'),
-(12, 'Jhazel Dela Cruz', 'kabfsheth', '14253', 5, 3, 'new', '2019-03-18 02:00:16', 'Pick-up', '0000-00-00'),
-(13, 'dsgreer', 'xghre', '45744', 5, 4, 'new', '2019-03-18 02:21:14', 'Deliver', '0000-00-00');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admininfo`
---
-ALTER TABLE `admininfo`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tblcnp`
---
-ALTER TABLE `tblcnp`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tblorders`
---
-ALTER TABLE `tblorders`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admininfo`
---
 ALTER TABLE `admininfo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `tblcnp`
---
-ALTER TABLE `tblcnp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `tblDogInfo`
+  MODIFY `id` varchar(20) NOT NULL;
 
---
--- AUTO_INCREMENT for table `tblorders`
---
-ALTER TABLE `tblorders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+ALTER TABLE `tblOwnerInfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
--- additional dog information ----
-ALTER TABLE `tblDogInfo`
-ADD COLUMN `name` varchar(50) DEFAULT NULL,
-CHANGE COLUMN `physical_characteristics` `color_markings` varchar(255) NOT NULL,
-DROP COLUMN `medical_records`,
-ADD COLUMN `last_vaccination_date` date DEFAULT NULL,
-ADD COLUMN `residence_last_3_months` varchar(255) DEFAULT NULL,
-CHANGE COLUMN `dog_pictures` `dog_pictures` text NOT NULL,
-ADD COLUMN `has_owner` varchar(10) NOT NULL;
